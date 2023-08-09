befec9586fa8c280925d87ba9d97dc6cc208f4e7
Rachel <gggggrea@gmail.com>
2023-08-09 20:57:56 +0100

    Add layout of the website

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,32 @@
+import About from "@/components/About";
+import Projects from "@/components/Projects";
+import Experience from "@/components/Experience";
+import Contact from "@/components/Contact";
+import CV from "@/components/CV";
+
 import styles from "@/styles/Home.module.css";
 
 export default function Home() {
-  return <>xxxx</>;
+  return (
+    <>
+      <header> about projects experience contact cv </header>
+      <main>
+        <section id="about">
+          <About />
+        </section>
+        <section id="projects">
+          <Projects />
+        </section>
+        <section id="experience">
+          <Experience />
+        </section>
+        <section id="contact">
+          <Contact />
+        </section>
+        <section id="cv">
+          <CV />
+        </section>
+      </main>
+    </>
+  );
 }
