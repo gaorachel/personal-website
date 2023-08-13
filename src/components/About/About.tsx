903067f4c18e3916b7391880e0dc014b13c3fedc
Rachel <gggggrea@gmail.com>
2023-08-13 17:57:48 +0100

    Update SkillChart

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -6,41 +6,119 @@ import dynamic from "next/dynamic";
 const ResponsiveLine = dynamic(() => import("@nivo/line").then((m) => m.ResponsiveLine), { ssr: false });
 
 import styles from "./About.module.css";
+import { Point } from "@nivo/line";
 
 function SkillChart() {
   const data = [
     {
       id: "skills",
-      color: "hsl(191, 70%, 50%)",
       data: [
         {
           x: "2020-01-01",
-          y: 107,
+          y: "python",
         },
         {
-          x: "2020-01-01",
-          y: 107,
+          x: "2020-02-01",
+          y: "pandas",
+        },
+        {
+          x: "2020-02-15",
+          y: "numpy",
+        },
+        {
+          x: "2020-06-01",
+          y: "openpyxl",
+        },
+        {
+          x: "2020-06-15",
+          y: "automate bring stuff",
+        },
+        {
+          x: "2020-10-01",
+          y: "sql",
+        },
+        {
+          x: "2020-12-01",
+          y: "data analysis",
+        },
+        {
+          x: "2023-02-01",
+          y: "web development",
+        },
+        {
+          x: "2023-02-01",
+          y: "javascript",
+        },
+        {
+          x: "2023-03-25",
+          y: "react",
+        },
+        {
+          x: "2023-04-11",
+          y: "css",
+        },
+
+        {
+          x: "2023-06-01",
+          y: "typescript",
+        },
+        {
+          x: "2023-07-15",
+          y: "nextjs",
         },
       ],
     },
   ];
 
+  function CustomTooltip({ point }: { point: Point }) {
+    const isFirstHalf = point.index < data[0].data.length / 2;
+    return (
+      <div
+        style={{
+          padding: "5px",
+          position: "absolute",
+          left: isFirstHalf ? 30 : 0,
+          right: isFirstHalf ? 0 : 30,
+        }}
+      >
+        {point.data.y as string}
+      </div>
+    );
+  }
+
   return (
     <ResponsiveLine
       data={data}
+      margin={{ top: 15, right: 15, bottom: 15, left: 15 }} // make sure showing up full chart
+      xFormat="time:%Y-%m-%d"
       xScale={{
-        type: "point",
-        // format: "%Y-%m-%d",
-        // precision: "Month",
-        // type: "time",
+        format: "%Y-%m-%d",
+        precision: "month",
+        type: "time",
+        useUTC: false,
       }}
       yScale={{
-        type: "linear",
-        min: "auto",
-        max: "auto",
-        stacked: true,
-        reverse: false,
+        type: "band",
       }}
+      axisBottom={{
+        // format: "%b %Y",
+        tickSize: 0,
+        // tickValues: "every 6 months",
+        // tickRotation: -37,
+        renderTick: () => <g />, // we don't want ticks to show
+      }}
+      axisLeft={{
+        tickSize: 0,
+        renderTick: () => <g />, // we don't want ticks to show
+      }}
+      curve="monotoneX"
+      enableGridX={false}
+      enableGridY={false}
+      useMesh={true}
+      tooltip={CustomTooltip}
+      pointSize={8}
+      enableCrosshair={false}
+      colors="#b48ead" // --aurora-5
     />
   );
 }
