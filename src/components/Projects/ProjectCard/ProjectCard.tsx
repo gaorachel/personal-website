16c62596fbe2c94bd6122a11415b47cd61dc9c66
Rachel <gggggrea@gmail.com>
2023-08-25 20:16:11 +0100

    Refactor code

--- src/components/Projects/ProjectCard/ProjectCard.tsx
+++ src/components/Projects/ProjectCard/ProjectCard.tsx
@@ -2,6 +2,7 @@ import { Chip } from "../Chip/Chip";
 import { RiExternalLinkLine } from "react-icons/ri";
 import { BsGithub } from "react-icons/bs";
 import { BiWifi0 } from "react-icons/bi";
+import Image from "next/image";
 
 import style from "./ProjectCard.module.css";
 
@@ -11,31 +12,36 @@ type ProjectCardType = {
   websiteURL: string;
   githubURL: string;
   techStack: string[];
+  image: string;
 };
 type ProjectCardProps = {
   project: ProjectCardType;
 };
 
 export function ProjectCard({ project }: ProjectCardProps) {
+  const imageStyle = { borderRadius: "6px" };
+
   return (
     <div className={style.card}>
-      <div className={style.project}>{project.name}</div>
+      <Image src={project.image} width={400} height={225} alt={`image of ${project.name}`} style={imageStyle} />
 
-      <div className={style.intro}> {project.intro} </div>
-      <div>
-        {project.techStack.map((el) => {
-          return <Chip key={el}> {el} </Chip>;
-        })}
-      </div>
-      <div>
-        <a href={project.websiteURL} className={style.url}>
-          {/* {project.name} <RiExternalLinkLine /> */}
-          website <RiExternalLinkLine />
-        </a>
-        <BiWifi0 />
-        <a href={project.githubURL} className={style.url}>
-          code <BsGithub />
-        </a>
+      <div className={style.text}>
+        <div className={style.project}>{project.name}</div>
+        <div className={style.intro}> {project.intro} </div>
+        <div className={style.chips}>
+          {project.techStack.map((el) => {
+            return <Chip key={el}> {el} </Chip>;
+          })}
+        </div>
+        <div>
+          <a href={project.websiteURL} className={style.url}>
+            website <RiExternalLinkLine />
+          </a>
+          <BiWifi0 />
+          <a href={project.githubURL} className={style.url}>
+            code <BsGithub />
+          </a>
+        </div>
       </div>
     </div>
   );
