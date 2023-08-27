e9f93b127acbf30e6bf6d68a2e594bcebd3ee4a8
Rachel <gggggrea@gmail.com>
2023-08-27 22:00:10 +0100

    Update code layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,6 @@
 import { Header } from "@/components/Header/Header";
 import { About } from "@/components/About/About";
 import { Projects } from "@/components/Projects/Projects";
-import { Experience } from "@/components/Experience/Experience";
 import { Sidebar } from "@/components/Sidebar/Sidebar";
 import { Journey } from "@/components/Journey/Journey";
 
@@ -12,18 +11,12 @@ export default function Home() {
   return (
     <>
       <Header />
-      <div className={styles.body}>
-        <main className={styles.main}>
-          <section id="about">{/* <About /> */}</section>
-          <section id="journey">
-            <Journey />
-          </section>
-          <section id="projects">
-            <Projects />
-          </section>
-        </main>
-        <Sidebar />
-      </div>
+      <main className={styles.main}>
+        <About />
+        <Journey />
+        <Projects />
+      </main>
+      <Sidebar />
     </>
   );
 }
