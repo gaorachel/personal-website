a64c81eb08abe45e772750b67d18f22df59fbe02
Rachel <gggggrea@gmail.com>
2023-08-27 22:52:29 +0100

    Update layout

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,6 +1,4 @@
 import JavaScriptIcon from "./skillIcons/javascript.svg";
-import NumPyIcon from "./skillIcons/numpy.svg";
-import PandasIcon from "./skillIcons/pandas.svg";
 import TypeScriptIcon from "./skillIcons/typescript.svg";
 import PythonIcon from "./skillIcons/python.svg";
 import BigQueryIcon from "./skillIcons/bigQuery.svg";
@@ -8,7 +6,6 @@ import SQLIcon from "./skillIcons/sql.svg";
 import ReactIcon from "./skillIcons/react.svg";
 import HTMLIcon from "./skillIcons/html.svg";
 import CSSIcon from "./skillIcons/css.svg";
-// import ReactIcon from "./skillIcons/react.svg";
 import NextJSIcon from "./skillIcons/nextjs.svg";
 
 import styles from "./About.module.css";
@@ -21,10 +18,8 @@ export function About() {
       <div className={styles.icons}>
         <JavaScriptIcon /> <TypeScriptIcon /> <ReactIcon /> <HTMLIcon /> <CSSIcon /> <NextJSIcon />
         <SQLIcon />
-        <BigQueryIcon style={{ height: "22px", width: "22px" }} />
+        <BigQueryIcon />
         <PythonIcon />
-        {/* <PandasIcon /> */}
-        {/* <NumPyIcon /> */}
       </div>
     </section>
   );
