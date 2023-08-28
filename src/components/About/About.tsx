94a9d9c04de351b83dfb23ef65af2e4d06e8f9aa
Rachel <gggggrea@gmail.com>
2023-08-28 22:36:15 +0100

    Add tooltips

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -9,16 +9,24 @@ import CSSIcon from "./skillIcons/css.svg";
 import NextJSIcon from "./skillIcons/nextjs.svg";
 
 import styles from "./About.module.css";
+import { Tooltip } from "react-tooltip";
 
 export function About() {
   return (
     <section className={styles.about} id="about">
       <h1 className={styles.name}> Rachel Gao </h1>
       <h2 className={styles.intro}> I am a self taught developer and this is my journey </h2>
+      <Tooltip id="skill-icons" place="bottom" />
       <div className={styles.icons}>
-        <JavaScriptIcon /> <TypeScriptIcon /> <ReactIcon /> <HTMLIcon /> <CSSIcon /> <NextJSIcon />
-        <SQLIcon /> <BigQueryIcon />
-        <PythonIcon />
+        <JavaScriptIcon data-tooltip-id="skill-icons" data-tooltip-content="JavaScript" />
+        <TypeScriptIcon data-tooltip-id="skill-icons" data-tooltip-content="TypeScript" />
+        <ReactIcon data-tooltip-id="skill-icons" data-tooltip-content="React" />
+        <HTMLIcon data-tooltip-id="skill-icons" data-tooltip-content="HTML" />
+        <CSSIcon data-tooltip-id="skill-icons" data-tooltip-content="CSS" />
+        <NextJSIcon data-tooltip-id="skill-icons" data-tooltip-content="NextJS" />
+        <SQLIcon data-tooltip-id="skill-icons" data-tooltip-content="SQL" />
+        <BigQueryIcon data-tooltip-id="skill-icons" data-tooltip-content="BigQuery" />
+        <PythonIcon data-tooltip-id="skill-icons" data-tooltip-content="Python" />
       </div>
     </section>
   );
